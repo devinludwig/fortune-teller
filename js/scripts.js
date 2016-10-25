054a1297a211b9d5c2e6a27854253bf716ed58ea
Devin Ludwig and Erica Nafziger <devin@>
2016-10-25 11:58:27 -0700

    add javascript looping to count checked boxes and display content depending on lucky vs unlucky count.

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,29 @@
 $(document).ready(function() {
-  
-})
+  $("form").submit(function(event) {
+    var unluckyArray = [];
+    var luckyArray = [];
+
+    event.preventDefault();
+    $("input:checkbox[name=unlucky]:checked").each(function(){
+      var unluckyInput = $(this).val();
+      unluckyArray.push(unluckyInput);
+    });
+    $("input:checkbox[name=lucky]:checked").each(function(){
+      var luckyInput = $(this).val();
+      luckyArray.push(luckyInput);
+
+    });
+    var unluckyTotal = unluckyArray.length;
+    alert(unluckyTotal);
+    var luckyTotal = luckyArray.length;
+    alert(luckyTotal);
+
+    if(unluckyTotal > luckyTotal) {
+      $("#unlucky").show();
+    } else if(unluckyTotal === luckyTotal) {
+      $("#neutral").show();
+    } else {
+      $("#lucky").show();
+    }
+  });
+});
